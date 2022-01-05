f2203c4ff2eec14b38caa9b66be6c3317ce565d7
chan.li <chan.li@ubtrobot.com>
2022-01-05 19:29:32 +0800

    feat:env

--- src/views/login/LoginIndex.vue
+++ src/views/login/LoginIndex.vue
@@ -1,7 +1,63 @@
 <template>
-  <h1>登录页123</h1>
+  <el-form
+    ref="formRef"
+    :rules="rules"
+    :model="ruleForm"
+    label-width="120px"
+    class="demo-dynamic"
+  >
+    <el-form-item
+      label="Activity name"
+      prop="name"
+    >
+      <el-input v-model="ruleForm.account" />
+    </el-form-item>
+    <el-form-item
+      label="Confirm"
+      prop="checkPass"
+    >
+      <el-input
+        v-model="ruleForm.pwd"
+        type="password"
+        autocomplete="off"
+      />
+    </el-form-item>
+    <el-form-item>
+      <el-button
+        type="primary"
+        @click="submitForm()"
+      >
+        登录
+      </el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import type { IElForm } from '@/types/element-plus'
+import { useRouter } from 'vue-router'
 
-<style lang="scss" scoped></style>
+import { reactive, ref } from 'vue'
+const formRef = ref<IElForm>()
+const ruleForm = reactive({
+  account: '',
+  pwd: ''
+})
+const rules = reactive({
+  account: [
+    { required: true, message: '请输入账号', trigger: 'change' }
+  ],
+  pwd: [
+    { required: true, message: '请输入密码', trigger: 'change' }
+  ]
+})
+const router = useRouter()
+const submitForm = async () => {
+  // 表单验证
+  const valid = await formRef.value?.validate()
+  if (!valid) {
+    return false
+  }
+  router.replace('/')
+}
+</script>
